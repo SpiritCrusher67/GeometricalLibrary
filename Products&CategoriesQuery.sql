7e2fa970cd6f14d306635e5213bfbcb0bd702f3b
SpiritCrusher67 <59704622+SpiritCrusher67@users.noreply.github.com>
2022-06-28 14:12:46 +0300

    Delete Products&CategoriesQuery.sql

--- Products&CategoriesQuery.sql
+++
@@ -1,3 +0,0 @@
-SELECT Products.Name AS ProguctName, Categories.Name AS CategoryName FROM Products
-LEFT JOIN ProductCategories ON ProductCategories.ProductId = Products.Id
-LEFT JOIN Categories ON ProductCategories.CategoryId = Categories.Id
